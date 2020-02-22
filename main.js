d988d65342bea28e4ba74691727766e1f5e1f8db
Hemiptera1 <55675342+Hemiptera1@users.noreply.github.com>
2020-02-22 19:40:45 -0300

    Fix + animación de Login y menu opciones

--- main.js
+++ main.js
@@ -3,12 +3,30 @@
 
 function visible() {
     let getMenuEmergente = document.querySelector(".menuEmergente");
-    getMenuEmergente.style.visibility = "visible";
+    getMenuEmergente.style.width = "100%";
+    getMenuEmergente.style.opacity = "1";
 }
 
 function invisible() {
     let getMenuEmergente = document.querySelector(".menuEmergente");
-    getMenuEmergente.style.visibility = "hidden";
+    getMenuEmergente.style.width = "0%";
+    getMenuEmergente.style.opacity = "0.5%";
 }
 
 /* FIN MENU EXPANDIBLE */
+
+/* LOGIN */
+
+function visibleLogin() {
+    let getLoginButton = document.querySelector(".loginExpandido");
+    getLoginButton.style.width = "100%";
+    getLoginButton.style.opacity = "1";
+}
+
+function invisibleLogin() {
+    let getLoginButton = document.querySelector(".loginExpandido");
+    getLoginButton.style.width = "0%";
+    getLoginButton.style.opacity = "0.5";
+}
+
+/* FIN LOGIN */
