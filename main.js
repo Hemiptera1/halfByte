50991b0897610ee24ffe4c704fec10ba59fd209e
Hemiptera1 <55675342+Hemiptera1@users.noreply.github.com>
2020-02-22 17:55:56 -0300

    Volví a copiar todo a la carpeta original, no puedo hacer que netlify lo levante desde static

---
+++ main.js
@@ -0,0 +1,14 @@
+
+/* MENU EXPANDIBLE */
+
+function visible() {
+    let getMenuEmergente = document.querySelector(".menuEmergente");
+    getMenuEmergente.style.visibility = "visible";
+}
+
+function invisible() {
+    let getMenuEmergente = document.querySelector(".menuEmergente");
+    getMenuEmergente.style.visibility = "hidden";
+}
+
+/* FIN MENU EXPANDIBLE */
