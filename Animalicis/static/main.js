d84aaca2d49a3321f7645a503b7b8481f937bbb3
Hemiptera1 <55675342+Hemiptera1@users.noreply.github.com>
2020-03-24 01:39:20 -0300

    Front-end: index: Creada la función que controla los botones del panel de busqueda (Funcionando - No terminado)

--- Animalicis/static/main.js
+++ Animalicis/static/main.js
@@ -48,44 +48,50 @@ function invisibleLogin() {
 
 /* FIN LOGIN */
 
-/* FORMULARIO REGISTRO DE ANIMALES */
+/* BOTONES DE SELECTORES */
 
-function poblar(s1, s2) {
-    var s1 = document.getElementById(s1);
-    var s1Value = s1.value;
-    var s2 = document.getElementById(s2);
-    var textoRaza = document.getElementById("textoRaza");
-    s2.innerHTML = "";
-    if(s1Value == "Perros") {
-        var opcionesEspecie = ["elijaUno|Elija uno","cimarron|Cimarron","rottweiler|Rottweiler","ovejeroAleman|Ovejero Aleman","mixto|Mixto"];
-        textoRaza.innerHTML = "¿De que raza es su perro?";
-    } else if(s1Value == "Gatos") {
-        var opcionesEspecie = ["elijaUno|Elija uno","largo|Largo","medio|Medio","largo|Corto","sinPelo|Sin pelo"]
-        textoRaza.innerHTML = "¿Que tan largo es el pelaje de su gato?";
-    } else if(s1Value == "Elija") {
-        textoRaza.innerHTML = "Escoja una especie primero";
+const botonPerros = document.getElementById("botonPerros");
+const botonGatos = document.getElementById("botonGatos");
+const botonOtros = document.getElementById("botonOtros");
+const botonRefugios = document.getElementById("botonRefugios");
+const botonImagen = document.getElementById("botonImagen");
+
+function cambiaActivos(argumento) {
+    console.log(argumento);
+    console.log(botonGatos.innerHTML);
+    if(argumento === botonPerros.innerHTML) {
+        botonPerros.classList.add("botonesActivos");
+        botonGatos.classList.remove("botonesActivos");
+        botonOtros.classList.remove("botonesActivos");
+        botonRefugios.classList.remove("botonesActivos");
+        botonImagen.src="/Animalicis/static/image/perroDesktop.png";
+        botonImagen.style.width="100%";
+        botonImagen.style.bottom="0";
+        botonImagen.style.left="0";
     }
-    for(var opcion in opcionesEspecie) {
-        var par = opcionesEspecie[opcion].split("|");
-        var nuevaOpcion = document.createElement("option");
-        nuevaOpcion.value = par[0];
-        nuevaOpcion.innerHTML = par[1];
-        s2.options.add(nuevaOpcion);
+    else if(argumento === botonGatos.innerHTML) {
+        botonPerros.classList.remove("botonesActivos");
+        botonGatos.classList.add("botonesActivos");
+        botonOtros.classList.remove("botonesActivos");
+        botonRefugios.classList.remove("botonesActivos");
+        botonImagen.src="/Animalicis/static/image/gatoSelectores.png";
+        botonImagen.style.width="115%";
+        botonImagen.style.bottom="-1.3rem";
+        botonImagen.style.left="-5.5rem";
+    }
+    else if(argumento === botonOtros.innerHTML) {
+        botonPerros.classList.remove("botonesActivos");
+        botonGatos.classList.remove("botonesActivos");
+        botonOtros.classList.add("botonesActivos");
+        botonRefugios.classList.remove("botonesActivos");
+    }
+    else if(argumento === botonRefugios.innerHTML) {
+        botonPerros.classList.remove("botonesActivos");
+        botonGatos.classList.remove("botonesActivos");
+        botonOtros.classList.remove("botonesActivos");
+        botonRefugios.classList.add("botonesActivos");
     }
 }
 
-const botonOriginal = document.getElementById("botonOriginal");
-const botonFalso = document.getElementById("botonFalso");
-const infoBoton = document.getElementById("infoBoton");
 
-botonFalso.addEventListener("click", function() {
-    botonOriginal.click();
-})
-
-botonOriginal.addEventListener("change", function() {
-    if(botonOriginal.value) {
-        infoBoton.innerHTML = "Subido :)";
-    }
-})
-
-/*  FIN DEL FORMULARIO DE REGISTRO DE ANIMALES */
+/* FIN DE BOTONES DE SELECTORES */
---
+++ Animalicis/static/registroJS.js
@@ -0,0 +1,41 @@
+/* FORMULARIO REGISTRO DE ANIMALES */
+
+function poblar(s1, s2) {
+    var s1 = document.getElementById(s1);
+    var s1Value = s1.value;
+    var s2 = document.getElementById(s2);
+    var textoRaza = document.getElementById("textoRaza");
+    s2.innerHTML = "";
+    if(s1Value == "Perros") {
+        var opcionesEspecie = ["elijaUno|Elija uno","cimarron|Cimarron","rottweiler|Rottweiler","ovejeroAleman|Ovejero Aleman","mixto|Mixto"];
+        textoRaza.innerHTML = "¿De que raza es su perro?";
+    } else if(s1Value == "Gatos") {
+        var opcionesEspecie = ["elijaUno|Elija uno","largo|Largo","medio|Medio","largo|Corto","sinPelo|Sin pelo"]
+        textoRaza.innerHTML = "¿Que tan largo es el pelaje de su gato?";
+    } else if(s1Value == "Elija") {
+        textoRaza.innerHTML = "Escoja una especie primero";
+    }
+    for(var opcion in opcionesEspecie) {
+        var par = opcionesEspecie[opcion].split("|");
+        var nuevaOpcion = document.createElement("option");
+        nuevaOpcion.value = par[0];
+        nuevaOpcion.innerHTML = par[1];
+        s2.options.add(nuevaOpcion);
+    }
+}
+
+const botonOriginal = document.getElementById("botonOriginal");
+const botonFalso = document.getElementById("botonFalso");
+const infoBoton = document.getElementById("infoBoton");
+
+botonFalso.addEventListener("click", function() {
+    botonOriginal.click();
+})
+
+botonOriginal.addEventListener("change", function() {
+    if(botonOriginal.value) {
+        infoBoton.innerHTML = "Subido :)";
+    }
+})
+
+/*  FIN DEL FORMULARIO DE REGISTRO DE ANIMALES */
